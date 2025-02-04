19d48ab21ee79879b919fc9965cef06f56502728
JanseR33 <154759639+JanseR33@users.noreply.github.com>
2025-02-04 10:32:15 +0100

    Add files via upload

--- Assets/Scripts/Systems/gamemanager.cs
+++ Assets/Scripts/Systems/gamemanager.cs
@@ -4,29 +4,42 @@ using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class GameManager : MonoBehaviour // Changed from class to MonoBehaviour
+using TMPro.Examples;
+public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<WaveConfiguration> waveConfigurations = new List<WaveConfiguration>();
-    [SerializeField] private Button startWaveButton; // Reference to the Start Wave button
+    [SerializeField] private Button startWaveButton; // The button that starts the wave (hopefully)
+    private List<Spawnpoint> allSpawnpoints = new List<Spawnpoint>();
     private bool isWaveActive = false;
-
+    
+    private void Start()
+    {
+        // this is a debug.log that will tell me when the button is pressed
+        startWaveButton.onClick.AddListener(() => Debug.Log("Button pressed!"));
+        // First, find all spawnpoints
+        allSpawnpoints = FindObjectsOfType<Spawnpoint>().ToList();
+    }
+    
     public void AddWave(WaveConfiguration waveConfig)
     {
         waveConfigurations.Add(waveConfig);
     }
-
     public void StartWave()
     {
+        if (startWaveButton == null)
+        {
+            Debug.Log("No start wave button assigned!"); // With this, I expect to be able to realize when the button is not assigned
+            return;
+        }
         if (isWaveActive)
         {
             Debug.Log("A wave is already in progress!");
             return;
         }
-
+        // this will send a warn message if there are no wave configurations
         if (!waveConfigurations.Any())
         {
-            Debug.Log("No wave configurations assigned!");
+            Debug.LogWarning("No wave configurations assigned!");
             return;
         }
 
@@ -64,13 +77,15 @@ public class GameManager : MonoBehaviour // Changed from class to MonoBehaviour
 
     private Spawnpoint FindSpawnpointByID(string id)
     {
-        foreach (Spawnpoint spawnpoint in UnityEngine.Object.FindObjectsOfType<Spawnpoint>())
+        Spawnpoint sp = allSpawnpoints.FirstOrDefault(sp => sp.SpawnpointID == id);
+        if (sp != null)
         {
-            if (spawnpoint.SpawnpointID == id)
-            {
-                return spawnpoint;
-            }
+            Debug.Log("Spawnpoint found!");
         }
-        return null;
+        else
+        {
+            Debug.LogWarning($"Spawnpoint with ID {id} not found!");
+        }
+        return sp;
     }
 }
